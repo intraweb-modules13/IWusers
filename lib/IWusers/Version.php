dd38d087884b7783e002501026e8d745798d6f21
aperezm <aperezm@e968f4be-1634-4921-b96b-e3dd0a78059a>
2011-03-24 10:46:01 +0000

    Refactoring modules to 1.3

--- lib/IWusers/Version.php
+++ lib/IWusers/Version.php
@@ -3,9 +3,9 @@ class IWusers_Version extends Zikula_Version
 {
     public function getMetaData() {
         $meta = array();
-        $meta['displayname'] = $this->$this->__("IWusers");
-        $meta['description'] = $this->$this->__("Improves the chances of users of the module.");
-        $meta['url'] = $this->$this->__("IWusers");
+        $meta['displayname'] = $this->__("IWusers");
+        $meta['description'] = $this->__("Improves the chances of users of the module.");
+        $meta['url'] = $this->__("IWusers");
         $meta['version'] = '3.0.0';
         $meta['securityschema'] = array('IWusers::' => '::');
         /*
