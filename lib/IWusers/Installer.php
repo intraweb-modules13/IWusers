ae91ed994981513d357ed5534671910cefe0602b
albert <aperez16@gmail.com>
2012-11-29 13:05:41 +0100

    Adapted to Agora

--- lib/IWusers/Installer.php
+++ lib/IWusers/Installer.php
@@ -2,7 +2,7 @@
 
 class IWusers_Installer extends Zikula_AbstractInstaller {
 
-    function Install() {
+    public function Install() {
         // Checks if module IWmain is installed. If not returns error
         $modid = ModUtil::getIdFromName('IWmain');
         $modinfo = ModUtil::getInfo($modid);
@@ -43,7 +43,7 @@ class IWusers_Installer extends Zikula_AbstractInstaller {
      * @author Albert Pérez Monfort (aperezm@xtec.cat)
      * @return bool true if successful, false otherwise
      */
-    function Uninstall() {
+    public function Uninstall() {
         // Delete module table
         DBUtil::dropTable('IWusers');
         DBUtil::dropTable('IWusers_friends');
@@ -67,75 +67,56 @@ class IWusers_Installer extends Zikula_AbstractInstaller {
      * @author Jaume Fernàndez Valiente (jfern343@xtec.cat)
      * @return bool true if successful, false otherwise
      */
-    function Upgrade($oldversion) {
-
-        $prefix = $GLOBALS['ZConfig']['System']['prefix'];
-
-        //Delete unneeded tables
-        DBUtil::dropTable('iw_users_aux');
-        DBUtil::dropTable('iw_users_import');
-
-        //Rename tables
-
-        if (!DBUtil::renameTable('iw_users', 'IWusers'))
-            return false;
-        if (!DBUtil::renameTable('iw_users_friends', 'IWusers_friends'))
-            return false;
+    public function Upgrade($oldversion) {
 
         // Add new columns
-        $c = "ALTER TABLE `{$prefix}_IWusers` ADD `iw_avatar` VARCHAR(50) NOT NULL";
+        $c = "ALTER TABLE `IWusers` ADD `iw_avatar` VARCHAR(50) NOT NULL";
         if (!DBUtil::executeSQL($c))
             return false;
-        $c = "ALTER TABLE `{$prefix}_IWusers` ADD `iw_newavatar` VARCHAR(50) NOT NULL";
+        $c = "ALTER TABLE `IWusers` ADD `iw_newavatar` VARCHAR(50) NOT NULL";
         if (!DBUtil::executeSQL($c))
             return false;
-        $c = "ALTER TABLE `{$prefix}_IWusers` ADD `iw_sex` TINYINT( 4 ) NOT NULL DEFAULT '0'";
+        $c = "ALTER TABLE `IWusers` ADD `iw_sex` TINYINT(4) NOT NULL DEFAULT '0'";
         if (!DBUtil::executeSQL($c))
             return false;
 
         // Delete unneded columns
         $c = array();
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_mobile` ";
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_fix` ";
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_parentsName` ";
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_address` ";
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_postal` ";
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_identifyCard` ";
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_refUser` ";
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_sendSMS` ";
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_active` ";
-        $c[] = "ALTER TABLE `{$prefix}_IWusers` DROP `iw_parentsEMail` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_mobile` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_fix` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_parentsName` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_address` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_postal` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_identifyCard` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_refUser` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_sendSMS` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_active` ";
+        $c[] = "ALTER TABLE `IWusers` DROP `iw_parentsEMail` ";
         foreach ($c as $sql) {
             DBUtil::executeSQL($sql);
         }
 
         // Modify column names
         $c = array();
-        $c = "ALTER TABLE `{$prefix}_IWusers` CHANGE `zk_obj_status` `pn_obj_status` VARCHAR(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT \'A\'";
-        $c = "ALTER TABLE `{$prefix}_IWusers` CHANGE `zk_cr_date` `pn_cr_date` DATETIME NOT NULL DEFAULT \'1970-01-01 00:00:00\'";
-        $c = "ALTER TABLE `{$prefix}_IWusers` CHANGE `zk_cr_uid` `pn_cr_uid` INT(11) NOT NULL DEFAULT \'0\'";
-        $c = "ALTER TABLE `{$prefix}_IWusers` CHANGE `zk_lu_date` `pn_lu_date` DATETIME NOT NULL DEFAULT \'1970-01-01 00:00:00\'";
-        $c = "ALTER TABLE `{$prefix}_IWusers` CHANGE `zk_lu_uid` `pn_lu_uid` INT(11) NOT NULL DEFAULT \'0\'";
+        $c = "ALTER TABLE `IWusers` CHANGE `zk_obj_status` `pn_obj_status` VARCHAR(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT \'A\'";
+        $c = "ALTER TABLE `IWusers` CHANGE `zk_cr_date` `pn_cr_date` DATETIME NOT NULL DEFAULT \'1970-01-01 00:00:00\'";
+        $c = "ALTER TABLE `IWusers` CHANGE `zk_cr_uid` `pn_cr_uid` INT(11) NOT NULL DEFAULT \'0\'";
+        $c = "ALTER TABLE `IWusers` CHANGE `zk_lu_date` `pn_lu_date` DATETIME NOT NULL DEFAULT \'1970-01-01 00:00:00\'";
+        $c = "ALTER TABLE `IWusers` CHANGE `zk_lu_uid` `pn_lu_uid` INT(11) NOT NULL DEFAULT \'0\'";
 
-        $c = "ALTER TABLE `{$prefix}_IWusers_friends` CHANGE `zk_obj_status` `pn_obj_status` VARCHAR(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT \'A\'";
-        $c = "ALTER TABLE `{$prefix}_IWusers_friends` CHANGE `zk_cr_date` `pn_cr_date` DATETIME NOT NULL DEFAULT \'1970-01-01 00:00:00\'";
-        $c = "ALTER TABLE `{$prefix}_IWusers_friends` CHANGE `zk_cr_uid` `pn_cr_uid` INT(11) NOT NULL DEFAULT \'0\'";
-        $c = "ALTER TABLE `{$prefix}_IWusers_friends` CHANGE `zk_lu_date` `pn_lu_date` DATETIME NOT NULL DEFAULT \'1970-01-01 00:00:00\'";
-        $c = "ALTER TABLE `{$prefix}_IWusers_friends` CHANGE `zk_lu_uid` `pn_lu_uid` INT(11) NOT NULL DEFAULT \'0\'";
+        $c = "ALTER TABLE `IWusers_friends` CHANGE `zk_obj_status` `pn_obj_status` VARCHAR(1) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT \'A\'";
+        $c = "ALTER TABLE `IWusers_friends` CHANGE `zk_cr_date` `pn_cr_date` DATETIME NOT NULL DEFAULT \'1970-01-01 00:00:00\'";
+        $c = "ALTER TABLE `IWusers_friends` CHANGE `zk_cr_uid` `pn_cr_uid` INT(11) NOT NULL DEFAULT \'0\'";
+        $c = "ALTER TABLE `IWusers_friends` CHANGE `zk_lu_date` `pn_lu_date` DATETIME NOT NULL DEFAULT \'1970-01-01 00:00:00\'";
+        $c = "ALTER TABLE `IWusers_friends` CHANGE `zk_lu_uid` `pn_lu_uid` INT(11) NOT NULL DEFAULT \'0\'";
         foreach ($c as $sql) {
             if (!DBUtil::executeSQL($sql))
                 return false;
         }
 
-        // Update module_vars table
-        // Update the name (keeps old var value)
-        $c = "UPDATE {$prefix}_module_vars SET z_modname = 'IWforms' WHERE z_bkey = 'iw_forms'";
-        if (!DBUtil::executeSQL($c)) {
-            return false;
-        }
 
         //Array de noms
-        $oldVarsNames = DBUtil::selectFieldArray("module_vars", 'name', "`z_modname` = 'IWforms'", '', false, '');
+        $oldVarsNames = DBUtil::selectFieldArray("module_vars", 'name', "`modname` = 'IWusers'", '', false, '');
 
         $newVarsNames = Array('friendsSystemAvailable', 'invisibleGroupsInList', 'usersCanManageName',
             'allowUserChangeAvatar', 'allowUserSetTheirSex', 'allowUserDescribeTheirSelves', 
